f6b01049d4bc16651e1f6107d0cd890d01487d74
Sarey Raslan <ssare2002@gmail.com>
2026-06-10 08:10:37 +0200

    Repair exporter modules as valid JavaScript

--- backend/src/midi-exporter.js
+++ backend/src/midi-exporter.js
@@ -1,16 +1,9 @@
-﻿$Root = "$HOME\Desktop\UAOS_ALL_AGENTS_FINAL_RUN\universal-arranger-os"
-cd $Root
-
-Write-Host "UAOS REAL ENGINE CONTINUE..." -ForegroundColor Cyan
-
-# 1. Fix backend exports
-@'
-export function exportMidiDraft(project){
+﻿export function exportMidiDraft(project){
   const song = project?.song?.song || [];
   let time = 0;
   const events = [];
 
-  for(const part of song){
+  for (const part of song) {
     events.push({
       time,
       type: "section",
