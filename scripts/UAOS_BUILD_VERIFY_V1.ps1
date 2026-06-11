39ae86db8543e9771b9a8118b1ed28d0cff7e8ca
Sarey Raslan <ssare2002@gmail.com>
2026-06-12 01:29:42 +0200

    Finalize UAOS public V1.2 deployment kit

--- scripts/UAOS_BUILD_VERIFY_V1.ps1
+++ scripts/UAOS_BUILD_VERIFY_V1.ps1
@@ -1,5 +1,5 @@
-﻿$Root="$HOME\Desktop\UAOS_ALL_AGENTS_FINAL_RUN\universal-arranger-os"
-Set-Location $Root
+﻿C:\Users\ssare\Desktop\UAOS_ALL_AGENTS_FINAL_RUN\universal-arranger-os="C:\Users\ssare\Desktop\UAOS_ALL_AGENTS_FINAL_RUN\universal-arranger-os"
+Set-Location C:\Users\ssare\Desktop\UAOS_ALL_AGENTS_FINAL_RUN\universal-arranger-os
 npm install --prefix uaos-live-clean
 npm run build --prefix uaos-live-clean
 git status
