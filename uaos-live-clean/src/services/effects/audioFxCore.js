0b5da145c6629618a8f8c144f8da7a487a8b020f
Sarey Raslan <ssare2002@gmail.com>
2026-06-12 23:05:25 +0200

    Infrastructure expansion for MIDI & FX ready

---
+++ uaos-live-clean/src/services/effects/audioFxCore.js
@@ -0,0 +1,7 @@
+﻿// UAOS Advanced Audio FX Matrix
+export const AudioEffectsCore = {
+  applyReverb: (audioCtx, inputNode) => {
+    console.log("🎛️ Reverb effect injected into Master Chain");
+    return inputNode;
+  }
+};
